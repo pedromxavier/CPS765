1a72792ce5a6497fffd895e2a5fde7bc5799244b
Pedro <pedromxavier@poli.ufrj.br>
2020-11-05 14:53:41 -0300

    Checkpoint

--- src/analysis.py
+++ src/analysis.py
@@ -11,35 +11,73 @@ ARGC = len(ARGV)
 
 ## Third-party
 from graph_tool.all import Graph
-import matplotlib.pyplot as plt
+from graph_tool.topology import label_components, shortest_distance
+## import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sn
+## import seaborn as sn
 
-for arg in ARGV[1:]:
+def components(g: Graph):
+    comp, *_ = label_components(g, directed=g.is_directed())
+    com = np.zeros(np.max(comp.a) + 1, dtype=int)
+    np.add.at(com, comp.a, 1)
+    return com
+
+def distances(g: Graph):
+    dist = shortest_distance(g, directed=g.is_directed())
+    return np.array([[y for y in x] for x in dist], dtype=int)
+
+def report(a: np.ndarray):
+    min_a = np.min(a)
+    max_a = np.max(a)
+    avg_a = np.mean(a)
+    med_a = np.median(a)
+    std_a = np.std(a)
+    return f'''  range = [{min_a:.1f}, {max_a:.1f}]
+    avg = {avg_a:.1f} ± {std_a:.1f}
+    med = {med_a:.1f}'''
+
+def get_graph(fname: str) -> Graph:
+    fdir = os.path.join(f'..', f'data') 
+    fpath = os.path.join(fdir, name)
+    if os.path.exists(fpath):
+        g = Graph()
+        g.load(file_name=f'../data/{name}', fmt='gt')
+        return g
+    else:
+        raise FileNotFoundError('Invalid Graph, options are:\n' + '\n'.join(os.listdir(fdir)))
+
+def analysis(name: str):
     t = clock()
-
     g = Graph()
-    g.load(file_name=f'../data/{arg}', fmt='gt')
+    g.load(file_name=f'../data/{name}', fmt='gt')
     
-    print(f'Data Loaded. Elapsed time: {clock() - t:.2f}s', end='\n')
+    print(f'Dados carregados. Tempo: {clock() - t:.2f}s', end='\n')
     v = g.get_vertices()
     e = g.get_edges()
-    print(f'Vertices = {len(v)}; Edges = {len(e)}', end='\n')
 
+    # Degree
     deg = g.get_total_degrees(v)
-    
-    min_deg = np.min(deg)
-    max_deg = np.max(deg)
-    avg_deg = np.mean(deg)
-    med_deg = np.median(deg)
-    std_deg = np.std(deg)
+    deg_rpt = report(deg)
 
-    print(f'''Degree:
-    min = {min_deg:.3f}
-    max = {max_deg:.3f}
-    avg = {avg_deg:.3f} ± {std_deg:.3f}
-    med = {med_deg:.3f}''')
-
-    print(f"Total Elapsed time: {clock() - t:.2f}s")
+    # Connected Components
+    com = components(g)
+    com_rpt = report(com)
+    td = clock()
+    # Distances
+    dis = distances(g)
+    dis_rpt = report(dis)
+    print(f'td = {clock() - td}')
     
-    del g
+    print(f'Vértices: {len(v)}; Arestas: {len(e)}; Componentes Conexas: {len(com)};', end='\n--\n')
+
+    print(f'Grau dos vértices:\n{deg_rpt}', end='\n--\n')
+
+    print(f'Tamanho das componentes conexas:\n{com_rpt}', end='\n--\n')
+
+    print(f'Distâncias:\n{dis_rpt}', end='\n--\n')
+
+    print(f"Tempo total: {clock() - t:.2f}s")
+
+for name in ARGV[1:]:
+    print(f'\n--------- {name:^20s} ---------\n')
+    analysis(name)
